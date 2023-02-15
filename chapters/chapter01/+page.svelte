85c729164726158f91d3ee22912453495663bb8d
dylan <dylan.hildenbrand@gmail.com>
2023-02-15 13:36:42 -0600

    Added code snippet from Chapter 1

---
+++ chapters/chapter01/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  let name = 'World';
+</script>
+<form>
+  <label for="name" >What is your name?</label>
+  <input type="text" name=”name” id=”name” bind:value={name} />
+</form>
+<h1>Hello, {name}!</h1>
