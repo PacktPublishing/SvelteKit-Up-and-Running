cb33f332eb9dbafce8b9b8fcdeee573624050ef0
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-02-27 10:45:44 -0600

    Chapter 4 code examples covering layouts

---
+++ full_project/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Nav from '$lib/Nav.svelte';
+</script>
+<div class='wrapper'>
+  <div class='nav'>
+    <Nav />
+  </div>
+  <div class='content'>
+    <slot></slot>
+  </div>
+  <div class='footer'>
+    This is my footer
+  </div>
+</div>
+<style>
+  .wrapper {
+    min-height: 100vh;
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+  } 
+  .footer {
+    text-align: center;
+    margin: 20px 0;
+  }
+</style>
