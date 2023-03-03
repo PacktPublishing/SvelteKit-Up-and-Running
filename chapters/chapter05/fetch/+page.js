6f957c14c972416791738e01e301b68845903f51
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-03-03 10:59:05 -0600

    Chapter 5 fetch example running in client only

---
+++ chapters/chapter05/fetch/+page.js
@@ -0,0 +1,12 @@
+const key = 'DEMO_KEY'; // your API key here
+
+export const ssr = false;
+export async function load() {
+  const pic = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+    .then(response => {
+      console.log('got response');
+      return response.json();
+    });
+
+  return {pic};
+}
