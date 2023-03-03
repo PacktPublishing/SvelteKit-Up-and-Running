1d10fa614a4c9a58fb05c2d05a4c0ed64a3f2327
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-03-03 09:58:23 -0600

    Run load() in client only by disabling ssr; chapter 5 first example.

--- full_project/src/routes/fetch/+page.js
+++ full_project/src/routes/fetch/+page.js
@@ -1,5 +1,6 @@
 const key = 'DEMO_KEY'; // your API key here
 
+export const ssr = false;
 export async function load() {
   const pic = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
     .then(response => {
