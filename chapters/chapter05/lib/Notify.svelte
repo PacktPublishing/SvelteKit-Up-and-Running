d1b0b44167aa956eeb8dbaeaeb180a2f49720e69
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-03-06 12:51:07 -0600

    Chapter 5 example code for loading with layouts

---
+++ chapters/chapter05/lib/Notify.svelte
@@ -0,0 +1,16 @@
+<script>
+import { invalidateAll } from '$app/navigation';
+export let count = 0;
+
+</script>
+<a href='/notifications' on:click={() => invalidateAll()}>
+  {count}
+</a>
+<style>
+  a {
+    padding: 15px;
+    color: white;
+    text-decoration: none;
+    background-color: #ea6262;
+  }
+</style>
