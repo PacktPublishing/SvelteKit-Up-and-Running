a8f05fba2754757232d2c5bb855361b9c3bcddaa
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-03-09 13:48:46 -0600

    Code snippets from chapter 5 about cookie based log in functionality

---
+++ chapters/chapter05/+layout.server.js
@@ -0,0 +1,29 @@
+export function load({ cookies }) {
+  let data = {
+    notifications: {
+      count: 3,
+      items: [
+        {
+          type: `comment`,
+          content: `Hi! I'm Dylan!`
+        },
+        {
+          type: `comment`,
+          content: `Hi Dylan. Nice to meet you!`
+        },
+        {
+          type: `comment`,
+          content: `Welcome to the chapter about load()!`
+        }
+      ]
+    }
+  };
+  if(cookies.get('identity') === '1') {
+    // lookup user ID in database
+    data.user = {
+      id: 1,
+      name: 'Dylan'
+    }
+  }
+  return data;
+}
---
+++ chapters/chapter05/login/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let data;
+</script>
+{#if data.user}
+  <p>
+    Welcome, {data.user.name}!
+  </p>
+{/if}
