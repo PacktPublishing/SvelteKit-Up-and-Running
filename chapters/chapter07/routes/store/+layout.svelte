c50b0ff11b9bba89e8a13f20f3184cd8fda6b594
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-04-21 10:35:46 -0500

    rest parameter code samples

---
+++ chapters/chapter07/routes/store/+layout.svelte
@@ -0,0 +1,6 @@
+<h2>Store</h2>
+<ul>
+  <li><a href="/store/locations">Locations</a></li>  
+  <li><a href="/store/products">Products</a></li>  
+</ul>
+<slot />
---
+++ chapters/chapter07/routes/store/products/[...details]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let data;
+</script>
+<h4>Product page</h4>
+{#if data.details}
+  <p class='red'>{data.details}</p>
+{:else}
+  <p>No product selected! Try clicking one or adding your own URL.
+{/if}
+
+<style>
+  .red {
+    color: red;
+    font-weight: bold;
+  }
+</style>
