b6262e7b6c1f2aa6e655b95fae9592a74ccbc78f
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-05-09 13:53:46 -0500

    Added modifications to fetch route from chapter 9

--- full_project/src/routes/(site)/fetch/+page.js
+++ full_project/src/routes/(site)/fetch/+page.js
@@ -1,7 +1,11 @@
+// import { browser } from '$app/environment';
 const key = 'DEMO_KEY'; // your API key here
 
 export const prerender = true;
 export function load() {
+  // if(browser) {
+  //  throw new Error('in the browser');
+  // }
   const pic = fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
     .then(response => {
       console.log('got response');
