7b1a8ca1afaf1e3901575f24b7ba1a64ccd36c72
dylan <dylan.hildenbrand@gmail.com>
2023-05-16 15:30:18 -0500

    Included code samples for Chapter 10

---
+++ chapters/chapter10/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script>
+  import '/src/reset.css';
+  import Nav from '$lib/Nav.svelte';
+  import Notify from '$lib/Notify.svelte';
+
+export let data;
+</script>
+<div class='wrapper'>
+  <div class='nav'>
+    <div class='menu'>
+      <Nav />
+    </div>
+    <div class='notifications'>
+      <Notify count={data.notifications.count}/>
+    </div>
+  </div>
+  <div class='content'>
+    <slot />
+  </div>
+  <div class='footer'>
+    This is my footer
+  </div>
+</div>
+<style>
+  .wrapper {
+    min-height: 100vh;
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+  } 
+  .nav {
+    text-align: center;
+  }
+  .menu {
+    display: inline-block;
+  }
+  .notifications {
+    float: right;
+  }
+  .footer {
+    text-align: center;
+    margin: 20px 0;
+  }
+</style>
