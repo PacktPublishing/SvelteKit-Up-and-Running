372e57257ee8e65b243b230bc8d0ebd32863342d
dylan <dylan.hildenbrand@gmail.com>
2023-06-07 13:49:52 -0500

    code related to tailwindcss adder found in appendix

--- full_project/svelte.config.js
+++ full_project/svelte.config.js
@@ -1,16 +1,21 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-    alias: {
-      db: '/src/db.js',
-      img: '/src/lib/images'
-    }
-  },
+		alias: {
+			db: '/src/db.js',
+			img: '/src/lib/images'
+		}
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
 };
 
 export default config;
-
-
