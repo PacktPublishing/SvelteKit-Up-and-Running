1114ca139ef2f81f4f8fb30c69974af6fa5bb9e2
dylan <dylan.hildenbrand@gmail.com>
2024-09-27 10:41:27 -0500

    async/await fetch example in chapter 3

--- chapters/chapter03/fetch/+page.js
+++ chapters/chapter03/fetch/+page.js
@@ -1,10 +1,11 @@
-const key = 'DEMO_KEY'; // your API key here
+const key = "DEMO_KEY"; // your API key here
 
-export function load() {
-  const pic = fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
-  .then(response => {
-    console.log('got response');
+export async function load() {
+  const pic = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${key}`,
+  ).then((response) => {
+    console.log("got response");
     return response.json();
   });
-  return {pic};
+  return { pic };
 }
